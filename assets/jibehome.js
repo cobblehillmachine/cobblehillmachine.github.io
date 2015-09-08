145920b22288938824263af2107b3e785737e2fb
maryhipp <maryhipp@gmail.com>
2015-09-08 11:51:06 -0400

    moved nav on mobile

--- assets/jibehome.js
+++ assets/jibehome.js
@@ -6,9 +6,9 @@ var advantages;
 $(window).load(function() {
   smoothScroll();
   offset = $('.nav.mobile').offset().top;
-  features = $('#features').offset().top;
-  homeBase = $('#home-base').offset().top;
-  advantages = $('#advantages').offset().top;
+  features = $('#features').offset().top - 67;
+  homeBase = $('#home-base').offset().top - 67;
+  advantages = $('#advantages').offset().top - 67;
   stickyNav();
   currentDiv();
 })
