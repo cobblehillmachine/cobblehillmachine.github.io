62773cb4fdbb62eafeffd036d4d66b94512bd28b
maryhipp <maryhipp@gmail.com>
2015-09-08 11:24:03 -0400

    moved nav on mobile

--- assets/jibehome.js
+++ assets/jibehome.js
@@ -2,7 +2,7 @@ var offset;
 
 $(window).load(function() {
   smoothScroll();
-  offset = $('.nav.mobile').offset().top - $('.nav.mobile').height();
+  offset = $('.nav.mobile').offset().top;
   stickyNav();
 })
 
